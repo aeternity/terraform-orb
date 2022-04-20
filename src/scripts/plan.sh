3792f320352dbd981bf8bf87d22da32b4a0a990c
Michal `acid` Bagrowski <michal@bagrowski.com>
2022-04-20 22:31:36 +0200

    add debug

--- src/scripts/plan.sh
+++ src/scripts/plan.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 echo "Creating dicectory for terraform out"
 mkdir -p "$(dirname "${TERRAFORM_OUT}")"
+echo $?
 echo "Generating secrets"
 envdir /secrets terraform -chdir=${TERRAFORM_PATH} plan \
        -lock-timeout=${TERRAFORM_LOCK_TIMEOUT} \
        -parallelism=${TERRAFORM_PARALLELISM} \
        -out=${TERRAFORM_OUT} \
        -var vault_addr=${VAULT_ADDR:?} | tee terraform-plan-info
-
+echo $?
+echo "Creating TERRAFORM_PLAN_INFO variable"
 TERRAFORM_PLAN_INFO=$(cat terraform-plan-info | grep Plan: | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g')a
+echo "Exporting TERRAFORM_PLAN_INFO variable"
 export TERRAFORM_PLAN_INFO
+echo $?
 curl -X POST \
           -H "Authorization: token ${GITHUB_TOKEN}" \
           -d '{
@@ -19,3 +23,4 @@ curl -X POST \
             "context": "terraform plan"
           }' \
           https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/statuses/${CIRCLE_SHA1}
+echo $?
