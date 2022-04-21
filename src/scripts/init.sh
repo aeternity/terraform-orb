47fb28b0cf7d30a5d5f73b2584764b9c25b053aa
Michal `acid` Bagrowski <michal@bagrowski.com>
2022-04-21 21:58:02 +0200

    fix

--- src/scripts/init.sh
+++ src/scripts/init.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo ${TERRAFORM_PATH}
-echo ${TERRAFORM_LOCK_TIMEOUT}
-terraform version
-terraform -chdir=${TERRAFORM_PATH} init -lock-timeout=${TERRAFORM_LOCK_TIMEOUT}
 envdir /secrets terraform -chdir=${TERRAFORM_PATH} init -lock-timeout=${TERRAFORM_LOCK_TIMEOUT}
